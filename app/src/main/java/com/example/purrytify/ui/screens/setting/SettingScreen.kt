35827fe60f5cea66e2bed578fbf03c6f608ecbdf
Muhammad Zaki <13522136@std.stei.itb.ac.id>
2025-03-27 15:46:24 +0700

    feat: add LibraryScreen, ProfileScreen, and update SettingScreen with basic layout

--- app/src/main/java/com/example/purrytify/ui/screens/setting/SettingScreen.kt
+++ app/src/main/java/com/example/purrytify/ui/screens/setting/SettingScreen.kt
@@ -1,2 +1,26 @@
 package com.example.purrytify.ui.screens.setting
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.purrytify.ui.theme.PurrytifyTheme
+
+
+@Composable
+fun SettingScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+
+    }
+}
+
+
+@Composable @Preview
+fun SettingScreenPreview() {
+    PurrytifyTheme {
+        SettingScreen()
+    }
+}
